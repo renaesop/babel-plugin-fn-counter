8ac03c8e88d7a8cb74095582ec436f2b28e80dd2
renaesop <renaesop@users.noreply.github.com>
2025-03-12 16:10:59 +0800

    Add test case for `__fn__counter` in `test/test.js`
    * **test/test.js**
      - Import `assert` from `assert` module
      - Import `transform` from `@babel/core` module
      - Import `plugin` from `../src/index.js`
      - Write a test case to verify `__fn__counter`, each key has right value
      - Execute the code in a sandbox and retrieve the `__fn__counter`'s value
      - Ensure functions in code string are called to trigger `__fn__counter`'s record
      - Set `fn_counter`'s key as `${state.file.opts.filename}:${functionName}:${path.node.loc.start.line}:${path.node.loc.start.column}`
    
    * **package-lock.json**
      - Add dependencies for `@babel/core` and `mocha`

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const { transform } = require('@babel/core');
 const plugin = require('../src/index.js');
+const { Script } = require('vm');
 
 describe('babel-plugin-fn-counter', () => {
   it('should count the number of functions correctly', () => {
@@ -8,9 +9,17 @@ describe('babel-plugin-fn-counter', () => {
       function a() {}
       const b = function() {};
       const c = () => {};
+      a();
+      b();
+      c();
     `;
     const output = transform(code, { plugins: [plugin] });
-    const { __fn__counter } = output.metadata;
-    assert.strictEqual(__fn__counter, 3);
+    const script = new Script(output.code);
+    const sandbox = { __fn__counter: {} };
+    script.runInNewContext(sandbox);
+    const { __fn__counter } = sandbox;
+    assert.strictEqual(__fn__counter['test/test.js:a:2:6'], 1);
+    assert.strictEqual(__fn__counter['test/test.js:b:3:12'], 1);
+    assert.strictEqual(__fn__counter['test/test.js:c:4:12'], 1);
   });
 });
